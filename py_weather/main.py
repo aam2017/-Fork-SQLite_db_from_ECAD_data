effc20cb34ed5380a29a30277eb09f059b33457a
Ingo Marquardt <ingo.marquardt@gmx.de>
2018-04-19 21:52:17 +0200

    First working version of neural net. Work in progress.

--- py_weather/main.py
+++ py_weather/main.py
@@ -26,13 +26,13 @@ from load_data import load_data
 # from sklearn.preprocessing import MinMaxScaler
 import tensorflow as tf
 # from sklearn.preprocessing import MinMaxScaler
-
+import matplotlib.pyplot as plt
 
 # -----------------------------------------------------------------------------
 # *** Define parameters
 
 # List of weather features to use (TG = mean temperature, PP = sea level
-# pressure, CC = cloud cover in oktas):
+# pressure, CC = cloud cover in oktas, RR = precipitation amount in 0.1 mm):
 lstFtr = ['CC', 'PP', 'RR', 'TG', 'TN', 'TX']
 
 # Input data path of text files (weather feature left open):
@@ -45,22 +45,43 @@ strFleIn = '{}_STAID{}.txt'
 varNumHdr = 21
 
 # Path of hdf5 file:
-strPthHd = '/media/sf_D_DRIVE/eu_weather/hdf5/data.hdf5'
+# strPthHd = '/media/sf_D_DRIVE/eu_weather/hdf5/data.hdf5'
+strPthHd = '/home/john/Documents/data.hdf5'
 
 # Load data starting from this data (YYYYMMMDD):
 varStrtDate = 19800101
 
-# Data until this date (YYYMMMDD):
+# Load data until this date (YYYMMMDD):
 varEndDate = 20180331
 
 # Predict weather based on past x days:
 varNumPast = 5
 
-# Predict weather for next x days:
+# Predict weather for xth day into the future (i.e. 1 for following day):
 varNumPre = 1
 
 # Station ID for which to predict:
 varBase = 432
+
+# Which feature to predict (index in list, see above):
+varFtrPrd = 2
+
+# Use last x days as test data set (the preceeding time period is used as
+# taining data set).
+varDaysTest = 365
+
+# Number of epochs:
+varEpch = 10
+
+# Output folder for plots:
+strPthPlot = '/media/sf_D_DRIVE/Sonstiges/Weather_Plots/'
+
+# Figure dpi:
+varDpi = 96
+
+# Figure size:
+varSizeX = 600
+varSizeY = 400
 # -----------------------------------------------------------------------------
 
 
@@ -84,156 +105,318 @@ else:
     fleHd = h5py.File(strPthHd, 'r')
     objData = fleHd['weather_data']
 
+# Data into memory:
+aryData = np.array(objData, dtype=np.float32)
+
 # Number of datasets (weather stations):
-varNumSta = objData.shape[0]
+varNumSta = aryData.shape[0]
 
 # Number of weather features:
-varNumFtr = objData.shape[1]
+varNumFtr = aryData.shape[1]
 
 # Number of days for which data is available:
-varNumDays = objData.shape[2]
+varNumDays = aryData.shape[2]
+
+# Invalid values coded as:
+varInvld = -9999.0
+
+# Replace invalid values by median value for respective feature:
+for idxFtr in range(varNumFtr):
+    varTmpFtrMne = np.median(aryData[:, idxFtr, :])
+    aryLgcInvl = np.equal(aryData[:, idxFtr, :], varInvld)
+    aryDataTmp = aryData[:, idxFtr, :]
+    aryDataTmp[aryLgcInvl] = varTmpFtrMne
+    aryData[:, idxFtr, :] = aryDataTmp
+    # print(np.sum(aryLgcInvl))
+
+# Test data set - use this data for prediction during test (i.e. all features
+# from all stations over the test preiod, e.g. last year):
+aryTest = np.array(aryData[:, :, -varDaysTest:],
+                   dtype=np.float32)
+
+# Training data set - use this data for training (i.e. all features from all
+# stations over the training period):
+aryTrain = np.array(aryData[:, :, 0:(-(varDaysTest + 10))],
+                    dtype=np.float32)
+
+# Normalise the training data to range 0 to 1 (separately for each feature):
+for idxFtr in range(varNumFtr):
+    vecTmp = aryTrain[:, idxFtr, :]
+    varTmpMin = np.min(vecTmp)
+    vecTmp = np.subtract(vecTmp, varTmpMin)
+    varTmpMax = np.max(vecTmp)
+    vecTmp = np.divide(vecTmp, varTmpMax)
+    aryTrain[:, idxFtr, :] = vecTmp
+
+# Normalise the test data to range 0 to 1 (separately for each feature):
+for idxFtr in range(varNumFtr):
+    vecTmp = aryTest[:, idxFtr, :]
+    varTmpMin = np.min(vecTmp)
+    vecTmp = np.subtract(vecTmp, varTmpMin)
+    varTmpMax = np.max(vecTmp)
+    vecTmp = np.divide(vecTmp, varTmpMax)
+    aryTest[:, idxFtr, :] = vecTmp
+
+# Normalise test data set:
+# varTmpMin = np.min(vecTest)
+# vecTest = np.subtract(vecTest, varTmpMin)
+# varTmpMax = np.max(vecTest)
+# vecTest = np.divide(vecTest, varTmpMax)
+
+# Test data set - predict this data during test (i.e. one feature from one
+# station over the test period, e.g. last year):
+vecTest = aryTest[varBase, varFtrPrd, :]
+
+# Training data set - predict this data during training (i.e. one feature from
+# one station over the training period):
+vecTrainPrd = aryTrain[varBase, varFtrPrd, :]
+
+# Reshape training data to aryTrain[(station * feature), days]
+aryTrain = np.reshape(aryTrain,
+                      ((aryTrain.shape[0] * aryTrain.shape[1]),
+                       aryTrain.shape[2])
+                      ).T
+
+# Reshape test data to aryTest[(station * feature), days]
+aryTest = np.reshape(aryTest,
+                     ((aryTest.shape[0] * aryTest.shape[1]),
+                      aryTest.shape[2])
+                     ).T
+
+del(aryData)
+del(aryDataTmp)
+del(aryLgcInvl)
+del(vecTmp)
 # -----------------------------------------------------------------------------
 
 
-## -----------------------------------------------------------------------------
-#
-#
-#
-## The data have the following shape: objData[station, weather-feature, day]
-#aryData = np.array(objData)
-#
-#vecBase = aryData[varBase, 0, :]
-#
-#aryData = np.reshape(aryData,
-#                     ((aryData.shape[0] * aryData.shape[1]),
-#                      aryData.shape[2])
-#                     )
-#
-#
-## Training and test data
-#train_start = 0
-#train_end = (varNumDays - (2 * 365))
-#test_start = train_end
-#test_end = varNumDays
-#data_train = aryData[:, np.arange(train_start, train_end)]
-#data_test = aryData[:, np.arange(test_start, test_end)]
-#
-#
-#
-#
-#
-## Scale data
-## ...
-#
-#
-## Placeholder
-#X = tf.placeholder(dtype=tf.float32, shape=[(varNumSta * varNumFtr), varNumPast])
-#Y = tf.placeholder(dtype=tf.float32, shape=[varNumFtr, varNumPre])
-#
-#
-## Initializers
-#sigma = 1
-#weight_initializer = tf.variance_scaling_initializer(mode="fan_avg",
-#                                                     distribution="uniform",
-#                                                     scale=sigma)
-#bias_initializer = tf.zeros_initializer()
-#
-#
-## Model architecture parameters
-#n_stocks = (varNumSta * varNumFtr)
-#n_neurons_1 = 1024
-#n_neurons_2 = 512
-#n_neurons_3 = 256
-#n_neurons_4 = 128
-#n_target = 1
-## Layer 1: Variables for hidden weights and biases
-#W_hidden_1 = tf.Variable(weight_initializer([n_stocks, n_neurons_1]))
-#bias_hidden_1 = tf.Variable(bias_initializer([n_neurons_1]))
-## Layer 2: Variables for hidden weights and biases
-#W_hidden_2 = tf.Variable(weight_initializer([n_neurons_1, n_neurons_2]))
-#bias_hidden_2 = tf.Variable(bias_initializer([n_neurons_2]))
-## Layer 3: Variables for hidden weights and biases
-#W_hidden_3 = tf.Variable(weight_initializer([n_neurons_2, n_neurons_3]))
-#bias_hidden_3 = tf.Variable(bias_initializer([n_neurons_3]))
-## Layer 4: Variables for hidden weights and biases
-#W_hidden_4 = tf.Variable(weight_initializer([n_neurons_3, n_neurons_4]))
-#bias_hidden_4 = tf.Variable(bias_initializer([n_neurons_4]))
-#
-## Output layer: Variables for output weights and biases
-#W_out = tf.Variable(weight_initializer([n_neurons_4, n_target]))
-#bias_out = tf.Variable(bias_initializer([n_target]))
-#
-#
-## Hidden layer
-#hidden_1 = tf.nn.relu(tf.add(tf.matmul(X, W_hidden_1), bias_hidden_1))
-#hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1, W_hidden_2), bias_hidden_2))
-#hidden_3 = tf.nn.relu(tf.add(tf.matmul(hidden_2, W_hidden_3), bias_hidden_3))
-#hidden_4 = tf.nn.relu(tf.add(tf.matmul(hidden_3, W_hidden_4), bias_hidden_4))
-#
-## Output layer (must be transposed)
-#out = tf.transpose(tf.add(tf.matmul(hidden_4, W_out), bias_out))
-#
-## Cost function
-#mse = tf.reduce_mean(tf.squared_difference(out, Y))
-#
-## Optimizer
-#opt = tf.train.AdamOptimizer().minimize(mse)
-#
-## Make Session
-#net = tf.Session()
-## Run initializer
-#net.run(tf.global_variables_initializer())
-#
-#
-#
-## Make Session
-#net = tf.Session()
-## Run initializer
-#net.run(tf.global_variables_initializer())
-#
-## Setup interactive plot
-##plt.ion()
-##fig = plt.figure()
-##ax1 = fig.add_subplot(111)
-##line1, = ax1.plot(y_test)
-##line2, = ax1.plot(y_test*0.5)
-##plt.show()
-#
-## Number of epochs and batch size
-#epochs = 10
-#batch_size = 256
-#
-#shuffle_indices = np.random.permutation(np.arange(varNumDays))
-#
-#for e in range(epochs):
-#
-#    # Shuffle training data
-#    shuffle_indices = np.random.permutation(np.arange(len(y_train)))
-#    X_train = X_train[shuffle_indices]
-#    y_train = y_train[shuffle_indices]
-#
-#    # Minibatch training
-#    for i in range(0, len(y_train) // batch_size):
-#        start = i * batch_size
-#        batch_x = X_train[start:start + batch_size]
-#        batch_y = y_train[start:start + batch_size]
-#        # Run optimizer with batch
-#        net.run(opt, feed_dict={X: batch_x, Y: batch_y})
-#
-#        # Show progress
-#        if np.mod(i, 5) == 0:
-#            # Prediction
-#            pred = net.run(out, feed_dict={X: X_test})
-#            line2.set_ydata(pred)
-#            plt.title('Epoch ' + str(e) + ', Batch ' + str(i))
-#            file_name = 'img/epoch_' + str(e) + '_batch_' + str(i) + '.jpg'
-#            plt.savefig(file_name)
-#            plt.pause(0.01)
-## Print final MSE after Training
-#mse_final = net.run(mse, feed_dict={X: X_test, Y: y_test})
-#print(mse_final)
-#
-## -----------------------------------------------------------------------------
-#
-#
-#
+# -----------------------------------------------------------------------------
+# *** Plot data to be predicted
+
+# Create figure:
+fgr01 = plt.figure(figsize=((varSizeX * 0.5) / varDpi,
+                            (varSizeY * 0.5) / varDpi),
+                   dpi=varDpi)
+
+# Create axis:
+axs01 = fgr01.add_subplot(111)
+
+# Plot depth profile for current input file:
+plt01 = axs01.plot(vecTest)
+
+# Output file name:
+strPltTmp = (strPthPlot + 'data_to_be_predicted.png')
+
+# Save figure:
+fgr01.savefig(strPltTmp,
+              facecolor='w',
+              edgecolor='w',
+              orientation='landscape',
+              transparent=False,
+              frameon=None)
+
+# Close figure:
+plt.close(fgr01)
+# -----------------------------------------------------------------------------
+            
+
+# -----------------------------------------------------------------------------
+# *** Train model
+
+# Placeholder
+objX = tf.placeholder(dtype=tf.float32,
+                      shape=[varNumPast, (varNumSta * varNumFtr)])
+objY = tf.placeholder(dtype=tf.float32, shape=[1])
+
+
+# Initializers
+varSigma = 1
+weight_initializer = tf.variance_scaling_initializer(mode="fan_avg",
+                                                     distribution="uniform",
+                                                     scale=varSigma)
+bias_initializer = tf.zeros_initializer()
+
+# Model architecture parameters
+n_input = (varNumSta * varNumFtr)
+n_neurons_1 = 8192
+n_neurons_2 = 4096
+n_neurons_3 = 2048
+n_neurons_4 = 1024
+n_neurons_5 = 512
+n_neurons_6 = 256
+n_neurons_7 = 128
+n_target = 1
+
+# Layer 1: Variables for hidden weights and biases
+W_hidden_1 = tf.Variable(weight_initializer([n_input, n_neurons_1]))
+bias_hidden_1 = tf.Variable(bias_initializer([n_neurons_1]))
+
+# Layer 2: Variables for hidden weights and biases
+W_hidden_2 = tf.Variable(weight_initializer([n_neurons_1, n_neurons_2]))
+bias_hidden_2 = tf.Variable(bias_initializer([n_neurons_2]))
+
+# Layer 3: Variables for hidden weights and biases
+W_hidden_3 = tf.Variable(weight_initializer([n_neurons_2, n_neurons_3]))
+bias_hidden_3 = tf.Variable(bias_initializer([n_neurons_3]))
+
+# Layer 4: Variables for hidden weights and biases
+W_hidden_4 = tf.Variable(weight_initializer([n_neurons_3, n_neurons_4]))
+bias_hidden_4 = tf.Variable(bias_initializer([n_neurons_4]))
+
+# Layer 5: Variables for hidden weights and biases
+W_hidden_5 = tf.Variable(weight_initializer([n_neurons_4, n_neurons_5]))
+bias_hidden_5 = tf.Variable(bias_initializer([n_neurons_5]))
+
+# Layer 6: Variables for hidden weights and biases
+W_hidden_6 = tf.Variable(weight_initializer([n_neurons_5, n_neurons_6]))
+bias_hidden_6 = tf.Variable(bias_initializer([n_neurons_6]))
+
+# Layer 7: Variables for hidden weights and biases
+W_hidden_7 = tf.Variable(weight_initializer([n_neurons_6, n_neurons_7]))
+bias_hidden_7 = tf.Variable(bias_initializer([n_neurons_7]))
+
+# Output layer: Variables for output weights and biases
+W_out = tf.Variable(weight_initializer([n_neurons_7, n_target]))
+bias_out = tf.Variable(bias_initializer([n_target]))
+
+# Hidden layer
+hidden_1 = tf.nn.relu(tf.add(tf.matmul(objX, W_hidden_1), bias_hidden_1))
+hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1, W_hidden_2), bias_hidden_2))
+hidden_3 = tf.nn.relu(tf.add(tf.matmul(hidden_2, W_hidden_3), bias_hidden_3))
+hidden_4 = tf.nn.relu(tf.add(tf.matmul(hidden_3, W_hidden_4), bias_hidden_4))
+hidden_5 = tf.nn.relu(tf.add(tf.matmul(hidden_4, W_hidden_5), bias_hidden_5))
+hidden_6 = tf.nn.relu(tf.add(tf.matmul(hidden_5, W_hidden_6), bias_hidden_6))
+hidden_7 = tf.nn.relu(tf.add(tf.matmul(hidden_6, W_hidden_7), bias_hidden_7))
+
+# Output layer (must be transposed)
+out = tf.transpose(tf.add(tf.matmul(hidden_7, W_out), bias_out))
+
+# Cost function
+mse = tf.reduce_mean(tf.squared_difference(out, objY))
+
+# Optimizer
+opt = tf.train.AdamOptimizer().minimize(mse)
+
+# Make Session
+net = tf.Session()
+
+# Run initializer
+net.run(tf.global_variables_initializer())
+
+# Setup interactive plot
+#plt.ion()
+#fig = plt.figure()
+#ax1 = fig.add_subplot(111)
+#line1, = ax1.plot(y_test)
+#line2, = ax1.plot(y_test*0.5)
+#plt.show()
+
+# Number of epochs and batch size
+# epochs = 10
+# batch_size = 256
+
+# Index of last day in training data set from which to make predictions (total
+# number of days in training data set minus lenght of prediction period minus
+# days in the future to predict):
+varLstDay = (aryTrain.shape[1] - varNumPast - varNumPre)
+
+# Indices of days from which to make prediction:
+vecIdx = np.arange(0, varLstDay, dtype=np.int32)
+
+for idxEpch in range(varEpch):
+    
+    print(('--Epoch: ' + str(idxEpch)))
+
+    # Shuffle training data indices:
+    vecIdxPerm = np.random.permutation(vecIdx)
+
+    # Loop through days:
+    for idxDay in range(vecIdxPerm.shape[0]):
+
+        # Data used for prediction:
+        aryTmpX = aryTrain[idxDay:(idxDay + varNumPast), :]
+
+        # Data point to predict:
+        varTmpY = np.array(vecTrainPrd[(idxDay + varNumPast + varNumPre)],
+                           ndmin=1)
+        
+        # Run optimizer with batch
+        net.run(opt, feed_dict={objX: aryTmpX, objY: varTmpY})
+
+        # Show progress
+        if np.mod(idxDay, 5) == 0:
+
+            # Number of days to predict from in test data set:
+            varNumTstDays = (vecTest.shape[0] - varNumPast - varNumPre)
+            
+            # Array for test predictions:
+            vecPrdTst = np.zeros((varNumTstDays), dtype=np.float32)
+            
+            # Loop through days in test data set:
+            for idxDayTst in range(varNumTstDays):
+
+                # aryTmpXtst = aryTest[idxDayTst:varNumPast, :]
+
+                # print(idxDayTst)
+
+                aryTestTmp = aryTest[idxDayTst:(idxDayTst + varNumPast), :]
+
+                # Prediction
+                pred = net.run(out, feed_dict={objX: aryTestTmp})
+
+                # print('type(pred)')
+                # print(type(pred))
+
+                # print('pred.shape')
+                # print(pred.shape)
+
+                # print('pred')
+                # print(pred)
+                
+                # `pred` has as many elements as there are features - why? Are
+                # all features predicted?
+
+                vecPrdTst[idxDayTst] = pred[0, varFtrPrd]
+                
+            # Error:
+            vecErr = np.subtract(vecTest[(varNumPast + varNumPre):],
+                                 vecPrdTst)
+
+            # Create figure:
+            fgr01 = plt.figure(figsize=((varSizeX * 0.5) / varDpi,
+                                        (varSizeY * 0.5) / varDpi),
+                               dpi=varDpi)
+        
+            # Create axis:
+            axs01 = fgr01.add_subplot(111)
+
+            # Plot depth profile for current input file:
+            plt01 = axs01.plot(vecErr)
+
+            # Output file name:
+            strPltTmp = (strPthPlot
+                         + 'prediction_epoch_'
+                         + str(idxEpch)
+                         + '_day_'
+                         + str(idxDay)
+                         + '.png')
+
+            # Save figure:
+            fgr01.savefig(strPltTmp,
+                          facecolor='w',
+                          edgecolor='w',
+                          orientation='landscape',
+                          transparent=False,
+                          frameon=None)
+        
+            # Close figure:
+            plt.close(fgr01)
+
+# Print final MSE after Training
+# mse_final = net.run(mse, feed_dict={objX: vecTest, objY: y_test})
+# print(mse_final)
+
+# -----------------------------------------------------------------------------
+
+
+
