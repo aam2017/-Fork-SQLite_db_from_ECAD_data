00963c89dca4d3f6339342108f429563490a772a
John <ingo.marquardt@gmx.de>
2018-06-11 01:09:26 +0200

    Recurrency in all hidden layers.

--- py_weather/main_rnn.py
+++ py_weather/main_rnn.py
@@ -62,8 +62,8 @@ varStrtDate = 19800101
 # Load data until this date (YYYMMMDD):
 varEndDate = 20180331
 
-# Predict weather based on past x days:
-varNumPast = 5
+# Remember layer states for tha past x days:
+varNumPast = 2
 
 # Predict weather for xth day into the future (i.e. 1 for following day):
 varNumPre = 1
@@ -223,8 +223,9 @@ for idxFtr in range(varNumFtr):
     # Create axis:
     axs01 = fgr01.add_subplot(1, varNumFtr, (idxFtr + 1))
 
-    # Plot depth profile for current input file:
+    # Plot data:
     plt01 = axs01.plot(aryTestPrd[idxFtr, :])
+    axs01.set_ylim([-2.0, 2.0])
 
 # Output file name:
 strPltTmp = (strPthPlot + 'data_to_be_predicted.png')
@@ -274,50 +275,80 @@ bias_initializer = tf.zeros_initializer()
 print('n_input')
 print(n_input)
 
+# Dimension of hidden layer (input data + state, number of neurons):
+lstDimHdn01 = [(n_input + (varNumPast * n_neurons_1)), n_neurons_1]
 # Layer 1: Variables for hidden weights and biases
-W_hidden_1 = tf.Variable(weight_initializer([(n_input + (varNumPast * n_neurons_1)), n_neurons_1]))
-bias_hidden_1 = tf.Variable(bias_initializer([n_neurons_1]))
-state_1 = tf.Variable(tf.zeros([1, (varNumPast * n_neurons_1)], dtype=tf.float32))
+W_hidden_1 = tf.Variable(weight_initializer(lstDimHdn01), dtype=tf.float32)
+bias_hidden_1 = tf.Variable(bias_initializer([n_neurons_1]), dtype=tf.float32)
+state_1 = tf.Variable(tf.zeros([1, (varNumPast * n_neurons_1)],
+                               dtype=tf.float32))
 
+# Dimension of hidden layer (input data + state, number of neurons):
+lstDimHdn02 = [(n_neurons_1 + (varNumPast * n_neurons_2)), n_neurons_2]
 # Layer 2: Variables for hidden weights and biases
-W_hidden_2 = tf.Variable(weight_initializer([n_neurons_1, n_neurons_2]))
-bias_hidden_2 = tf.Variable(bias_initializer([n_neurons_2]))
+W_hidden_2 = tf.Variable(weight_initializer(lstDimHdn02), dtype=tf.float32)
+bias_hidden_2 = tf.Variable(bias_initializer([n_neurons_2]), dtype=tf.float32)
+state_2 = tf.Variable(tf.zeros([1, (varNumPast * n_neurons_2)],
+                               dtype=tf.float32))
 
+# Dimension of hidden layer (input data + state, number of neurons):
+lstDimHdn03 = [(n_neurons_2 + (varNumPast * n_neurons_3)), n_neurons_3]
 # Layer 3: Variables for hidden weights and biases
-W_hidden_3 = tf.Variable(weight_initializer([n_neurons_2, n_neurons_3]))
-bias_hidden_3 = tf.Variable(bias_initializer([n_neurons_3]))
+W_hidden_3 = tf.Variable(weight_initializer(lstDimHdn03), dtype=tf.float32)
+bias_hidden_3 = tf.Variable(bias_initializer([n_neurons_3]), dtype=tf.float32)
+state_3 = tf.Variable(tf.zeros([1, (varNumPast * n_neurons_3)],
+                               dtype=tf.float32))
 
+# Dimension of hidden layer (input data + state, number of neurons):
+lstDimHdn04 = [(n_neurons_3 + (varNumPast * n_neurons_4)), n_neurons_4]
 # Layer 4: Variables for hidden weights and biases
-W_hidden_4 = tf.Variable(weight_initializer([n_neurons_3, n_neurons_4]))
-bias_hidden_4 = tf.Variable(bias_initializer([n_neurons_4]))
+W_hidden_4 = tf.Variable(weight_initializer(lstDimHdn04), dtype=tf.float32)
+bias_hidden_4 = tf.Variable(bias_initializer([n_neurons_4]), dtype=tf.float32)
+state_4 = tf.Variable(tf.zeros([1, (varNumPast * n_neurons_4)],
+                               dtype=tf.float32))
 
+# Dimension of hidden layer (input data + state, number of neurons):
+lstDimHdn05 = [(n_neurons_4 + (varNumPast * n_neurons_5)), n_neurons_5]
 # Layer 5: Variables for hidden weights and biases
-W_hidden_5 = tf.Variable(weight_initializer([n_neurons_4, n_neurons_5]))
-bias_hidden_5 = tf.Variable(bias_initializer([n_neurons_5]))
+W_hidden_5 = tf.Variable(weight_initializer(lstDimHdn05), dtype=tf.float32)
+bias_hidden_5 = tf.Variable(bias_initializer([n_neurons_5]), dtype=tf.float32)
+state_5 = tf.Variable(tf.zeros([1, (varNumPast * n_neurons_5)],
+                               dtype=tf.float32))
 
+# Dimension of hidden layer (input data + state, number of neurons):
+lstDimHdn06 = [(n_neurons_5 + (varNumPast * n_neurons_6)), n_neurons_6]
 # Layer 6: Variables for hidden weights and biases
-W_hidden_6 = tf.Variable(weight_initializer([n_neurons_5, n_neurons_6]))
-bias_hidden_6 = tf.Variable(bias_initializer([n_neurons_6]))
+W_hidden_6 = tf.Variable(weight_initializer(lstDimHdn06), dtype=tf.float32)
+bias_hidden_6 = tf.Variable(bias_initializer([n_neurons_6]), dtype=tf.float32)
+state_6 = tf.Variable(tf.zeros([1, (varNumPast * n_neurons_6)],
+                               dtype=tf.float32))
 
+# Dimension of hidden layer (input data + state, number of neurons):
+lstDimHdn07 = [(n_neurons_6 + (varNumPast * n_neurons_7)), n_neurons_7]
 # Layer 7: Variables for hidden weights and biases
-W_hidden_7 = tf.Variable(weight_initializer([n_neurons_6, n_neurons_7]))
-bias_hidden_7 = tf.Variable(bias_initializer([n_neurons_7]))
+W_hidden_7 = tf.Variable(weight_initializer(lstDimHdn07), dtype=tf.float32)
+bias_hidden_7 = tf.Variable(bias_initializer([n_neurons_7]), dtype=tf.float32)
+state_7 = tf.Variable(tf.zeros([1, (varNumPast * n_neurons_7)],
+                               dtype=tf.float32))
 
 # Output layer: Variables for output weights and biases
-W_out = tf.Variable(weight_initializer([n_neurons_7, n_target]))
-bias_out = tf.Variable(bias_initializer([n_target]))
+W_out = tf.Variable(weight_initializer([n_neurons_7, n_target]),
+                    dtype=tf.float32)
+bias_out = tf.Variable(bias_initializer([n_target]), dtype=tf.float32)
 # bias_out = tf.Variable(bias_initializer([1, varNumFtr]))
 
 # Hidden layer
 hidden_1 = tf.nn.relu(tf.add(tf.matmul(tf.concat([objX, state_1], 1),
                                        W_hidden_1),
                              bias_hidden_1))
+# Update state:
 state_1 = tf.concat(
                     [tf.reshape(hidden_1, [1, n_neurons_1]),
-                     state_1[:, 0:(-n_neurons_1)]],  # tf.reshape(state_1[0:-n_neurons_1], [state_1.shape])],
+                     state_1[:, 0:(-n_neurons_1)]],
                     1)
 
-print(type(tf.concat([objX, state_1], 1)))
+print('tf.concat([objX, state_1], 1).shape')
+print(tf.concat([objX, state_1], 1).shape)
 
 print('hidden_1.shape')
 print(hidden_1.shape)
@@ -325,12 +356,59 @@ print(hidden_1.shape)
 print('state_1.shape')
 print(state_1.shape)
 
-hidden_2 = tf.nn.relu(tf.add(tf.matmul(hidden_1, W_hidden_2), bias_hidden_2))
-hidden_3 = tf.nn.relu(tf.add(tf.matmul(hidden_2, W_hidden_3), bias_hidden_3))
-hidden_4 = tf.nn.relu(tf.add(tf.matmul(hidden_3, W_hidden_4), bias_hidden_4))
-hidden_5 = tf.nn.relu(tf.add(tf.matmul(hidden_4, W_hidden_5), bias_hidden_5))
-hidden_6 = tf.nn.relu(tf.add(tf.matmul(hidden_5, W_hidden_6), bias_hidden_6))
-hidden_7 = tf.nn.relu(tf.add(tf.matmul(hidden_6, W_hidden_7), bias_hidden_7))
+hidden_2 = tf.nn.relu(tf.add(tf.matmul(tf.concat([hidden_1, state_2], 1),
+                                       W_hidden_2),
+                             bias_hidden_2))
+# Update state:
+state_2 = tf.concat(
+                    [tf.reshape(hidden_2, [1, n_neurons_2]),
+                     state_2[:, 0:(-n_neurons_2)]],
+                    1)
+
+hidden_3 = tf.nn.relu(tf.add(tf.matmul(tf.concat([hidden_2, state_3], 1),
+                                       W_hidden_3),
+                             bias_hidden_3))
+# Update state:
+state_3 = tf.concat(
+                    [tf.reshape(hidden_3, [1, n_neurons_3]),
+                     state_3[:, 0:(-n_neurons_3)]],
+                    1)
+
+hidden_4 = tf.nn.relu(tf.add(tf.matmul(tf.concat([hidden_3, state_4], 1),
+                                       W_hidden_4),
+                             bias_hidden_4))
+# Update state:
+state_4 = tf.concat(
+                    [tf.reshape(hidden_4, [1, n_neurons_4]),
+                     state_4[:, 0:(-n_neurons_4)]],
+                    1)
+
+hidden_5 = tf.nn.relu(tf.add(tf.matmul(tf.concat([hidden_4, state_5], 1),
+                                       W_hidden_5),
+                             bias_hidden_5))
+# Update state:
+state_5 = tf.concat(
+                    [tf.reshape(hidden_5, [1, n_neurons_5]),
+                     state_5[:, 0:(-n_neurons_5)]],
+                    1)
+
+hidden_6 = tf.nn.relu(tf.add(tf.matmul(tf.concat([hidden_5, state_6], 1),
+                                       W_hidden_6),
+                             bias_hidden_6))
+# Update state:
+state_6 = tf.concat(
+                    [tf.reshape(hidden_6, [1, n_neurons_6]),
+                     state_6[:, 0:(-n_neurons_6)]],
+                    1)
+
+hidden_7 = tf.nn.relu(tf.add(tf.matmul(tf.concat([hidden_6, state_7], 1),
+                                       W_hidden_7),
+                             bias_hidden_7))
+# Update state:
+state_7 = tf.concat(
+                    [tf.reshape(hidden_7, [1, n_neurons_7]),
+                     state_7[:, 0:(-n_neurons_7)]],
+                    1)
 
 # Output layer (must be transposed)
 out = tf.transpose(tf.add(tf.matmul(hidden_7, W_out), bias_out))
@@ -447,8 +525,9 @@ for idxEpch in range(varEpch):
                 # Create axis:
                 axs01 = fgr01.add_subplot(1, varNumFtr, (idxFtr + 1))
 
-                # Plot depth profile for current input file:
+                # Plot prediction error:
                 plt01 = axs01.plot(aryErr[idxFtr, :])
+                axs01.set_ylim([-2.0, 2.0])
 
             # Output file name:
             strPltTmp = (strPthPlot
